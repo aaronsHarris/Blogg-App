4d4c880e9618d3ec5b553d2a50766f9dced6970a
Aaron Harris <aaronsharris@gmail.com>
2021-10-04 14:53:26 -0500

    domain name

--- .history/db/connection_20211004145300.js
+++ .history/db/connection_20211004145300.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 let MONGODB_URI =
   process.env.PROD_MONGODB ||
-  "mongodb://127.0.0.1:27017/aaronawesomebloggdb"
+  "mongodb://127.0.0.1:27017/aaronawesomebloggDB"
 
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
