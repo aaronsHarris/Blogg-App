8bf831905d552be2d92174fb8d9627fb38eb994d
Aaron Harris <aaronsharris@gmail.com>
2021-10-05 13:26:42 -0500

    added font and spacing to nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -5,11 +5,11 @@ export const Nav = () => {
   return (
     <nav>
       <div className="nav">
-      <NavLink className="logo" to="/">  The Awesome Blogg</NavLink>
-      <div>
-        <NavLink className="link" to="/">Posts</NavLink>
-        <NavLink className="link" to="/posts">Create Post</NavLink>
-      </div>
+        <NavLink className="logo" to="/">  The Awesome Blogg</NavLink>
+        <div className="links">
+          <NavLink className="link" to="/">Posts</NavLink>
+          <NavLink className="link" to="/posts">Create Post</NavLink>
+        </div>
       </div>
     </nav>
   )
