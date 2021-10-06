4374a57a2c40e86f85b7779a9d35a8f293588eba
Tyler Washington <tylerwashington@Tylers-MacBook-Air.local>
2021-10-06 10:57:51 -0400

    post and posts components and screens

--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import "./Posts.css"
+import Layout from "../../components/Layout/Layout.jsx"
+import { getPosts } from "../../services/posts"
+import Post from '../../components/Post/Post'
 export default function Posts() {
-  return (
-    <div>
+  const [posts, setPosts] = useState([]);
 
-    </div>
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const allPosts = await getPosts();
+      setPosts(allPosts)
+      console.log(allPosts)
+    }
+    fetchPosts()
+  }, [])
+
+
+  return (
+    <Layout>
+      <div className="posts">
+        {posts.map((post) => {
+          return (
+            <Post
+              title={post.title}
+              author={post.author}
+              content={post.content}
+              imgUrl={post.imgUrl}
+            />
+          )
+        })}
+      </div>
+    </Layout>
   )
 }
